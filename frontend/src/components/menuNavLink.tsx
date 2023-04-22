473e3714f285528d1c43cf5c7bace92731b879bd
Harmary <maria.h4rlamova@yandex.ru>
2023-04-22 23:09:36 +0300

    did pretty menuLink on hover

--- frontend/src/components/menuNavLink.tsx
+++ frontend/src/components/menuNavLink.tsx
@@ -1,5 +1,7 @@
-import { Typography } from "@mui/material"
+import { MenuItem, Typography } from "@mui/material"
 import { NavLink } from "react-router-dom"
+import { css } from '@emotion/css'
+import theme from "../theme";
 
 type MenuNavLinkProps = {
     path: string,
@@ -7,10 +9,28 @@ type MenuNavLinkProps = {
     name: string,
 }
 
+const style = css({
+    textDecoration: "none",
+    '& :hover': {
+        color: theme.palette.primary.main,
+        textShadow: '0px 5px 67px #5F44B1'
+    },
+    '& :visited':{
+        color: '#000'
+    },
+    '& :active':{
+        color: '#000'
+    },
+    '& :focus':{
+        color: '#000'
+    }
+});
+
 export default function MenuNavLink(props: MenuNavLinkProps) {
     return <>
-        <NavLink style={{ textDecoration: "none" }} to={props.path}>
+        <NavLink className={style} to={props.path}>
             <Typography color={props.theme === "white" ? "16161A" : "#fffffe"} textAlign="center" variant='body1'>{props.name}</Typography>
         </NavLink>
+
     </>
 }
