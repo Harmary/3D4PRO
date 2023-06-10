08c0a0e81610c8d752814ec8d39889c44e448868
Harmary <maria.h4rlamova@yandex.ru>
2023-06-10 14:58:04 +0300

    change 3d scene

--- frontend/src/components/3dScene.jsx
+++ frontend/src/components/3dScene.jsx
@@ -1,30 +1,40 @@
-import { Canvas, useLoader } from "react-three-fiber";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { Environment, OrbitControls } from "@react-three/drei";
 import { Suspense } from "react";
-import gltfmodel from '../assets/gltf/sourse/home.gltf';
+import { Canvas } from "@react-three/fiber";
+import { Center, useGLTF, OrbitControls, Environment, Sky } from "@react-three/drei";
+import * as THREE from "three";
 
+function Model(link) {
+  const { scene } = useGLTF(`https://s3.timeweb.com/373825a7-49aec453-9ac5-487f-a13b-54d1d68bc0de/models/be1f5dac-199d-41f3-b479-e40859dd5045/e37bd034-20bc-47ba-8f16-c2590124c257/sourse/домик.gltf`);
+  return <primitive object={scene} />;
+}
 
-const Model = (link) => {
-  const gltf = useLoader(GLTFLoader, gltfmodel);
+function Scene(props) {
   return (
-    <>
-      <primitive object={gltf.scene} scale={5} />
-    </>
-  );
-};
-
-
-function Scene(model) {
-  return (
-    <Canvas style={{ background: "#171717", width: "inherit", height: 500 }}>
-      <Suspense fallback={null}>
-        <Model link={model} />
-        <OrbitControls />
-        <Environment preset='forest' background />
+    <div
+      style={{ height: "100%" }}
+      camera={{ position: new THREE.Vector3(8, 5, 40) }}
+    >
+      <Suspense>
+        <Canvas
+          shadows
+          camera={{ position: [0, -10, 100], fov: 50 }}
+          dpr={[1, 2]}
+        >
+          <Environment background preset="forest"/>
+          <Center>
+            <Model link={props.link} />
+          </Center>
+          <OrbitControls
+            makeDefault
+            minPolarAngle={0}
+            maxPolarAngle={Math.PI / 1.75}
+          />
+        </Canvas>
       </Suspense>
-    </Canvas>
+    </div>
   );
 }
+
+
 
 export default Scene;
