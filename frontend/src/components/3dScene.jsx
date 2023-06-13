e6af0083085040b1a1f643426370d2432f54936c
Harmary <maria.h4rlamova@yandex.ru>
2023-06-13 19:23:32 +0300

    add props to sceene

--- frontend/src/components/3dScene.jsx
+++ frontend/src/components/3dScene.jsx
@@ -1,28 +1,35 @@
-import { Suspense } from "react";
+import { startTransition, Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { Center, useGLTF, OrbitControls, Environment, Sky } from "@react-three/drei";
+import {
+  Center,
+  useGLTF,
+  OrbitControls,
+  Environment,
+  Sky,
+} from "@react-three/drei";
 import * as THREE from "three";
 
-function Model(link) {
-  const { scene } = useGLTF(`https://s3.timeweb.com/373825a7-49aec453-9ac5-487f-a13b-54d1d68bc0de/models/be1f5dac-199d-41f3-b479-e40859dd5045/e37bd034-20bc-47ba-8f16-c2590124c257/sourse/домик.gltf`);
-  return <primitive object={scene} />;
+function Model(props) {
+  const { scene } = useGLTF(props.model);
+
+  return <primitive scale={5} object={scene} />;
 }
 
 function Scene(props) {
   return (
     <div
-      style={{ height: "100%" }}
+      style={{ height: "100%", width: "100%" }}
       camera={{ position: new THREE.Vector3(8, 5, 40) }}
     >
-      <Suspense>
+      <Suspense startTransition fallback={null}>
         <Canvas
           shadows
           camera={{ position: [0, -10, 100], fov: 50 }}
           dpr={[1, 2]}
         >
-          <Environment background preset="forest"/>
+          <Environment background preset='forest' />
           <Center>
-            <Model link={props.link} />
+            <Model model={props.link} />
           </Center>
           <OrbitControls
             makeDefault
@@ -34,7 +41,5 @@ function Scene(props) {
     </div>
   );
 }
-
-
 
 export default Scene;
